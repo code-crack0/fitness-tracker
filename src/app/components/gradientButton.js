56bfe3a0df90d095f49f23553bc045b954a82603
Saeed <sbm7640@outlook.com>
2024-10-17 16:47:21 +0400

    Co-authored-by: Samad Sayed <code-crack0@users.noreply.github.com>

---
+++ src/app/components/gradientButton.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function gradientButton() {
+  return (
+    <View>
+      <Text>gradientButton</Text>
+    </View>
+  )
+}
