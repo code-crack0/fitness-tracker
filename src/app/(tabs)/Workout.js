bd4bad4cd4eedfae96b397f1b88797cb2f866cf5
Saeed <sbm7640@outlook.com>
2024-10-20 20:19:45 +0400

    added gradient buttons, modified Workout.js

--- src/app/(tabs)/Workout.js
+++ src/app/(tabs)/Workout.js
@@ -1,59 +1,106 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Image, Dimensions, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { TextInput, Button, Text, Title } from "react-native-paper";
 import { useRouter } from "expo-router";
 // needs to be installed
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native";
+import { GradientBUtton } from "react-linear-gradient-button";
 
-
-const categories = ['Arms', 'Chest', 'Legs', 'Shoulders', 'Abs']
+const categories = ["Arms", "Chest", "Legs", "Shoulders", "Abs"];
 
 export default function Workout() {
-  const [activeCategory, setActiveCategory] = useState('Action');
-  
-    return (
-    <LinearGradient colors={['#2193b0', '#6dd5ed']} 
-    className="w-full flex-1 ">
-      <SafeAreaView >
-        <View className="container">
-            {/* workout categories */}
-          <View className="mt-3">
-            <Text
-              className="ml-4 mt-3 mb-4 text-[25px] font-bold "
-            >
-                Browse Workouts
-            </Text> 
-            <View className="pl-4">
-                <ScrollView horizontal 
-                showsHorizontalScrollIndicator={false}>
-                    {
-                        categories.map(item=>{
-                            if(item==activeCategory){
-                                //Show that button is clicked
-                                
-                            }else{
-                                //Show that button is as is, unclicked
-                                return (
-                                    <TouchableOpacity 
-                                    onPress={()=> setActiveCategory}
-                                    key={item}
-                                    className="bg-rose-400 p-3 px-4
-                                    rounded-full mr-1">
-                                        <Text className="font-bold"
-                                        >{item}</Text>
-                                    </TouchableOpacity>
-                                )
-                            }
+  const [activeCategory, setActiveCategory] = useState("Action");
+  const featured = [
+    {
+      id: 1,
+      title: 'Diamond Push Up',
+      image: require('../assets/images/diamonPushUp.png'),
+      desc: 'A bodyweight exercise that targets the chest, triceps, and shoulders, with an added emphasis on the triceps. Your hands are positioned close together, forming a diamond shape by touching your thumbs and index fingers. This hand placement increases the intensity on the triceps while still engaging the chest and core.',
+    },
+    {
+      id: 2,
+      title: 'Squat',
+      image: require('../assets/images/squat.jpg'),
+      desc: 'A bodyweight exercise that focuses on strengthening the legs, targeting the quads, hamstrings, and glutes. It involves starting in a squat position, where the knees are bent, and the hips are pushed back as if sitting on an invisible chair, then driving through the heels to stand back up. It\'s a fundamental movement for building lower body strength, and improving balance and mobility. ',
+    },
+    {
+      id: 3,
+      title: 'Diamond Push Up',
+      image: require('../assets/images/diamonPushUp.png'),
+      desc: 'A bodyweight exercise that targets the chest, triceps, and shoulders, with an added emphasis on the triceps. Your hands are positioned close together, forming a diamond shape by touching your thumbs and index fingers. This hand placement increases the intensity on the triceps while still engaging the chest and core. ',
+    },
+    {
+      id: 4,
+      title: 'Bicep Curl',
+      image: require('../assets/images/bicepCurl.png'),
+      desc: 'A bicep curl is a strength exercise where you lift a weight by bending your elbow, targeting the biceps for arm strength and muscle definition.',
+    },
+  ]
 
-                        })
-                    }
+  return (
+    <LinearGradient colors={["#2193b0", "#6dd5ed"]} className="w-full flex-1 ">
+      <SafeAreaView>
+        <View className="container">
+          {/* workout categories */}
+          <View className="mt-3">
+            <Text className="ml-4 mt-3 mb-4 text-[25px] font-bold ">
+              Browse Workouts
+            </Text>
+            <View className="pl-4">
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {categories.map((item) => {
+                  if (item == activeCategory) {
+                    //Show that button is clicked
+                    return (
+                      <GradientBUtton
+                        key={item}
+                        containerClass="mr-2"
+                        value={item}
+                      />
+                    );
+                  } else {
+                    //Show that button is as is, unclicked
+                    return (
+                      <TouchableOpacity
+                        onPress={() => setActiveCategory}
+                        key={item}
+                        className="bg-rose-400 p-3 px-4
+                                    rounded-full mr-1"
+                      >
+                        <Text className="font-bold">{item}</Text>
+                      </TouchableOpacity>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+
+          {/* features workout section */}
+          <View className="mt-3 space-y-4">
+            <Text
+              style={{ color: storeColors.text }}
+              className="ml-4 text-lg font-bold"
+            >
+              Featured Exercises
+            </Text>
+            <View className="pl-4">
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  {
+
+                  }
                 </ScrollView>
             </View>
           </View>
         </View>
-
       </SafeAreaView>
     </LinearGradient>
   );
--- src/app/components/gradientButton.js
+++ src/app/components/gradientButton.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default function gradientButton() {
+export default function gradientButton(props) {
   return (
-    <View>
-      <Text>gradientButton</Text>
-    </View>
-  )
+    <LinearGradient
+      colors={["#17658a", "#4aa9bf"]}
+      end={{ x: 1, y: 1 }}
+      start={{ x: 0.1, y: 0.2 }}
+      className={"rounded-full ${props.containerClass}"}
+    >
+      <TouchableOpacity className={'p-3 px-4 s{props.buttonClass}'}>
+        <Text className="text-white font-bold">
+          {props.value}
+        </Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
 }
